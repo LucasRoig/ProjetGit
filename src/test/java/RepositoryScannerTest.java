7c2156ba5d88268a0332491a5b375cff23631ce3
LucasRoig <lucas1.roig@gmail.com>
2016-02-05 00:00:21 +0100

    Ajout des tests unitaires pour la classe RepositoryScanner

---
+++ src/test/java/RepositoryScannerTest.java
@@ -0,0 +1,34 @@
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import model.RepositoryScanner;
+
+/**
+ * 
+ * @author lucas
+ *Classe de test pour RepositoryScanner
+ *Ici on vérifie simplement que le RepositoryScanner récupère la bonne liste de
+ *valeurs de hash.
+ */
+public class RepositoryScannerTest {
+
+	@Test
+	public void test() {
+		File file = new File("src/test/ressources/RepertoireTestGit/.git/objects");
+		RepositoryScanner repositoryScanner = new RepositoryScanner(file);
+		ArrayList<String> expectedList = new ArrayList<>();
+		expectedList.add("5d2a619a227dd05e3151e6bedb1bed53cfd37a16");
+		expectedList.add("b6e799b3a0c8b3614c0a955994a80a9ea50ea204");
+		expectedList.add("d0e5aa177f2dc29ba5e8ef31dba60efdd0b24a3a");
+		ArrayList<String> actual = new ArrayList<>();
+		for (String string : repositoryScanner) {
+			actual.add(string);
+		}
+		assertTrue("Test du contenu du RepositoryScanner",expectedList.equals(actual));
+	}
+}
