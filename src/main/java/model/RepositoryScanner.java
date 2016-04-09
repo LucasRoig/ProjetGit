386d8342791b53966af17cf3e9c922a4afc51ffb
LucasRoig <lucas1.roig@gmail.com>
2016-04-09 16:05:10 +0200

    Ajout de la classe TreeEntry

--- src/main/java/model/RepositoryScanner.java
+++ src/main/java/model/RepositoryScanner.java
@@ -10,74 +10,83 @@ import org.eclipse.jgit.internal.storage.file.PackFile;
 import org.eclipse.jgit.internal.storage.file.PackIndex.MutableEntry;
 
 /**
- * Scanne un dossier object généré par git et récupère les hash de chaque objet git
+ * Scanne un dossier object généré par git et récupère les hash de chaque objet
+ * git
+ * 
  * @author Bruce Baumann
  * @author Ulysse Baumann
  * @author Lucas Roig
  */
-public class RepositoryScanner implements Iterable<String>{
+public class RepositoryScanner implements Iterable<String> {
 	private String path;
 	private ArrayList<String> hashList = new ArrayList<>();
-	
+
 	/**
 	 * Instancie un objet RepositoryScanner et initialise la variable path
-	 * @param path  Le chemin de du répertoire objects à scanner
+	 * 
+	 * @param path
+	 *            Le chemin de du répertoire objects à scanner
 	 */
 	public RepositoryScanner(String path) {
 		this(new File(path));
 	}
-	
+
 	public RepositoryScanner(File file) {
 		this.path = file.getAbsolutePath();
 		this.scan();
 	}
-	
+
 	@Override
 	public Iterator<String> iterator() {
 		return hashList.iterator();
 	}
-	
+
 	/**
-	 * Scanne le dossier ciblé par la variable path et stocke les hashs obtenus dans la liste hashList
+	 * Scanne le dossier ciblé par la variable path et stocke les hashs obtenus
+	 * dans la liste hashList
 	 */
-	private void scan(){
+	private void scan() {
 		File repertoireObjet = new File(path);
 		FileFilter filter = new FileFilter() {
 			@Override
 			public boolean accept(File pathname) {
-				return pathname.isDirectory() && !pathname.getName().contains("pack") && !pathname.getName().contains("info");
+				return pathname.isDirectory() && !pathname.getName().contains("pack")
+						&& !pathname.getName().contains("info");
 			}
 		};
 		for (File sousDossier : repertoireObjet.listFiles(filter)) {
 			scanSubDirectory(sousDossier);
 		}
-		
+
 		readPackFiles();
 	}
-	
+
 	/**
-	 * Scanne un dossier et ajoute les noms des fichiers trouvés à la liste hashList.
-	 * @param directory Le dossier à scanner
+	 * Scanne un dossier et ajoute les noms des fichiers trouvés à la liste
+	 * hashList.
+	 * 
+	 * @param directory
+	 *            Le dossier à scanner
 	 */
 	private void scanSubDirectory(File directory) {
 		FileFilter filter = new FileFilter() {
-			
+
 			@Override
 			public boolean accept(File pathname) {
 				return !pathname.isDirectory();
 			}
 		};
-		
+
 		for (File gitObject : directory.listFiles(filter)) {
 			hashList.add(directory.getName() + gitObject.getName());
 		}
 	}
-	
+
 	/**
-	 * Ajoute les hash des GitObjects �tants dans des packs � la liste hashList.
+	 * Ajoute les hash des GitObjects étants dans des packs à la liste hashList.
 	 */
-	private void readPackFiles(){
-		File repertoirePack = new File(path+"/pack");
+	private void readPackFiles() {
+		File repertoirePack = new File(path + "/pack");
 		FilenameFilter filter = new FilenameFilter() {
 			@Override
 			public boolean accept(File dir, String name) {
@@ -85,11 +94,9 @@ public class RepositoryScanner implements Iterable<String>{
 			}
 		};
 		for (File pack : repertoirePack.listFiles(filter)) {
-			if(pack.getAbsolutePath().contains(".pack")){
-				PackFile p = new PackFile(pack, 0);
-				for (MutableEntry mutableEntry : p) {
-					hashList.add(mutableEntry.toObjectId().getName());
-				}
+			PackFile p = new PackFile(pack, 0);//TODO : c'est quoi extension ?
+			for (MutableEntry mutableEntry : p) {
+				hashList.add(mutableEntry.toObjectId().getName());
 			}
 		}
 	}
--- src/main/java/model/Tree.java
+++ src/main/java/model/Tree.java
@@ -1,38 +1,53 @@
 package model;
 
+import java.util.ArrayList;
+
+import com.sun.corba.se.impl.encoding.OSFCodeSetRegistry.Entry;
+
 public class Tree extends GitObject{
+	private ArrayList<TreeEntry> treeEntriesList = new ArrayList<>();
+	
 	public Tree(String hash, byte[] b) {
 		super(hash, "");
 		this.type = GitObjectType.Tree;
 
 		//Decode les données spécifiques aux trees
 		StringBuffer buffer = new StringBuffer();
+		StringBuffer lineBuffer = new StringBuffer(); // contient une seule ligne de caractères
 		boolean trad = false;
 		int cpt = 0;
 		for(byte by :b){
 			if(by == 0){
 				if(!trad){
 					buffer.append((char)32);
+					lineBuffer.append((char)32);
 					cpt = 20;
 				}
-				trad = !trad;
+				trad = true;
 			}else{
 				if(trad){
 					String string = Integer.toHexString(by);
 					if (string.length() > 2) // TODO:vérifier l'utilité de cette ligne
 						string = string.substring(string.length() - 2);
 					buffer.append(string);
+					lineBuffer.append(string);
 					cpt --;
 					if (cpt <= 0){
-						trad = !trad;
+						trad = false;
 						buffer.append("\n");
+						treeEntriesList.add(new TreeEntry(lineBuffer.toString()));
+						lineBuffer.setLength(0);
 					}
 				}else{
+					lineBuffer.append((char)by);
 					buffer.append((char)by);
 				}
 			}
 		}
 		this.setRawData(buffer.toString());
 	}
-
+	
+	public ArrayList<TreeEntry> getTreeEntriesList() {
+		return treeEntriesList;
+	}
 }
